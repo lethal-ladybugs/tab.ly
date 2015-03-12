02be84c7b60b0052461b10734abb0949d6d6bdd0
alexwang949 <alex_cellist@yahoo.com>
2015-03-11 20:56:21 -0400

    fixed enum

--- tably/app/models/job.rb
+++ tably/app/models/job.rb
@@ -2,5 +2,7 @@ class Job < ActiveRecord::Base
 
 	has_many :ratings
 	belongs_to :user
+
+	enum status: [ :waiter, :bartender, :host, :barista ]
 	
 end
--- tably/db/migrate/20150311231721_create_jobs.rb
+++ tably/db/migrate/20150311231721_create_jobs.rb
@@ -2,7 +2,7 @@ class CreateJobs < ActiveRecord::Migration
   def change
     create_table :jobs do |t|
     	t.integer :user_id
-  		enum status: [ :bartender, :barista, :waiter, :host ]
+    	t.integer :status
       t.timestamps null: false
     end
   end
