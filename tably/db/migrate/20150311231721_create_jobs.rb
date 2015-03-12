d86a9a6f25c4fff3256f4de908d5b450b5c5b7ff
alexwang949 <alex_cellist@yahoo.com>
2015-03-11 20:44:32 -0400

    added enum "status"

--- tably/db/migrate/20150311231721_create_jobs.rb
+++ tably/db/migrate/20150311231721_create_jobs.rb
@@ -2,7 +2,7 @@ class CreateJobs < ActiveRecord::Migration
   def change
     create_table :jobs do |t|
     	t.integer :user_id
-  		enum position: [:bartender, :barista, :waiter, :host]
+  		enum status: [ :bartender, :barista, :waiter, :host ]
       t.timestamps null: false
     end
   end
