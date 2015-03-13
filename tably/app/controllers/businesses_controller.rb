213b0be2534e072e75bb19d99eaa6505fca337d6
Alex <alexadeschamps@gmail.com>
2015-03-13 16:31:47 -0400

    testing yelp:

--- tably/app/controllers/businesses_controller.rb
+++ tably/app/controllers/businesses_controller.rb
@@ -1,6 +1,13 @@
 class BusinessesController < ApplicationController
 
 	def index
+		binding.pry
+		search_params = { 
+			location: params['business']['zipcode']
+		}
+		binding.pry
+		results = Yelp.client.search('San Francisco').to_json
+		@parsed = JSON.parse(results)
 		@businesses = Business.all
 	end
 
