8fb4d9dfa009d7620d83c6123f6630c94ad6de90
alexwang949 <alex_cellist@yahoo.com>
2015-03-13 15:36:37 -0400

    changed

--- tably/app/controllers/tests_controller.rb
+++ tably/app/controllers/tests_controller.rb
@@ -1,9 +1,10 @@
 class TestsController < ApplicationController
 
 	def show
-		params = { term: 'coffee,beer,food', limit: 20 }
+		params = { term: 'coffee&beer&food', limit: 20 }
 		results = Yelp.client.search('San Francisco', params).to_json
-		@parsed = JSON.parse(results)
+		parsed = JSON.parse(results)
+		binding.pry
 	end
 
 end
