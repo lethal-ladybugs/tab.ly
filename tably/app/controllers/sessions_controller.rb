976869a3eaa735004b49b60934903a074a0786b9
Sonya Pinero <sonyapinero@gmail.com>
2015-03-14 19:08:55 -0400

    auth

--- tably/app/controllers/sessions_controller.rb
+++ tably/app/controllers/sessions_controller.rb
@@ -1,21 +1,21 @@
 class SessionsController < ApplicationController
 
 	def new 
-		user = User.new
+		@user = User.new
 	end
 
 	def create 
 	
-		user = User.find_by_email(params[:user][:email])
+		@user = User.find_by_email(params[:user][:email])
 
-			if user && user.authenticate(params[:user][:password])
+			if @user && @user.authenticate(params[:user][:password])
 			
 				token = SecureRandom.urlsafe_base64
 			
 				session[:session_token] = token 
 
-				user.session_token = token 
-				user.save 
+				@user.session_token = token 
+				@user.save 
 
 				redirect_to root_url 
 
@@ -26,14 +26,16 @@ class SessionsController < ApplicationController
 
 	def destroy
 
-    user = current_user
 
-    if user
-
+    @user = current_user
+		
+    if @user
 			session[:session_token] = nil
 
-      user.session_token = nil
-      user.save
+      @user.session_token = nil
+      Pry.start(binding)
+      @user.save
+    
     end
 
     redirect_to root_url
