dab7811c66c9801128283726307626c583c690a2
Alex <alexadeschamps@gmail.com>
2015-03-17 09:46:10 -0400

    added some styles, changed businesscontroller to default to new york

--- tably/app/controllers/businesses_controller.rb
+++ tably/app/controllers/businesses_controller.rb
@@ -2,13 +2,16 @@ class BusinessesController < ApplicationController
 
 	def index
 		terms = params['business']['name'].split.join(',')
+		
 		search_params = {
 			limit: 10,
 			term: params['business']['name'],
 			category_filter: 'restaurants,bars,cafes,icecream',
 			radius_filter: 30000
 		}
-		results = Yelp.client.search(params['business']['zipcode'], search_params).to_json
+		params['business']['zipcode'] = nil if params['business']['zipcode'] == ''
+		location = params['business']['zipcode'] || 'New York'
+		results = Yelp.client.search(location, search_params).to_json
 		@parsed = JSON.parse(results)
 		@businesses = Business.all
 	end
@@ -17,7 +20,6 @@ class BusinessesController < ApplicationController
 		results = Yelp.client.business(params[:id]).to_json
 		@business = JSON.parse(results)
 		@employees = Job.where(yelp_id: params[:id])
-
 	end
 
 end
