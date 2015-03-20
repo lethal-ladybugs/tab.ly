68f75fa9dbb2274f3ea7d00a23e69b27418755ca
Alex <alexadeschamps@gmail.com>
2015-03-20 11:48:41 -0400

    added in email uniquenss

--- tably/app/models/user.rb
+++ tably/app/models/user.rb
@@ -4,7 +4,8 @@ class User < ActiveRecord::Base
 	attr_accessor :password_confirmation
 	
 	has_secure_password 
-	validates :email, presence: true 
+	validates :email, presence: true
+	validates :email, uniqueness: true
 	validates :password, 
 						:presence => true, 
 						:length => { :minimum => 6},
