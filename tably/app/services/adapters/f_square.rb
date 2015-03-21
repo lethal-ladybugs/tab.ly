4867a536bdbfd3e4e8415c2b28ed8a4de230fe56
Alex <alexadeschamps@gmail.com>
2015-03-21 11:31:37 -0400

    foursquare api working

--- tably/app/services/adapters/f_square.rb
+++ tably/app/services/adapters/f_square.rb
@@ -7,7 +7,7 @@ class F_square
 
   def get(loc, terms)
     conn = Faraday.new(url: "https://api.foursquare.com/")
-    response = conn.get("v2/venues/search?client_id=M33X4KUQHS41MLWGZGNQVBOFGU3HOZIVJDXBV5YNR2NZARDB&client_secret=IE3YZZUOE34UP1HIO2ZQC1KGZTQAXQN3HUXUW2XVDO2JGBYR&v=20130815&intent=browse&radius=1000&near=" + loc + '&query=' + terms)
+    response = conn.get("v2/venues/search?client_id=M33X4KUQHS41MLWGZGNQVBOFGU3HOZIVJDXBV5YNR2NZARDB&client_secret=IE3YZZUOE34UP1HIO2ZQC1KGZTQAXQN3HUXUW2XVDO2JGBYR&v=20130815&intent=browse&radius=1000&limit=5&near=" + loc + '&query=' + terms)
     if response.success?
       normalize_response(JSON.parse response.body )
     else
@@ -17,9 +17,34 @@ class F_square
 
   private
   def normalize_response(response)
-  	response
-  	binding.pry
+  	# normalized = {
+
+  	# }
+  	venues = []
+  	response['response']['venues'].each do |venue|
+  		each = {
+  			name: venue['name'],
+  			address: venue['location']['address'],
+  			phone: venue['contact']['formattedPhone'],
+  			photo: getPhoto(venue["id"])
+  		}
+  		venues << each
+  	end
+  	venues
+  	# binding.pry
     # Create hash from response
+  end
+
+  def getPhoto(id)
+  	conn = Faraday.new(url: "https://api.foursquare.com/")
+    response = conn.get("v2/venues/#{id}/photos?client_id=M33X4KUQHS41MLWGZGNQVBOFGU3HOZIVJDXBV5YNR2NZARDB&client_secret=IE3YZZUOE34UP1HIO2ZQC1KGZTQAXQN3HUXUW2XVDO2JGBYR&limit=1&v=20130815")
+    photo_obj = JSON.parse response.body
+    if urls = photo_obj["response"]["photos"]["items"][0]
+    	urls = photo_obj["response"]["photos"]["items"][0]
+  		url = urls["prefix"] + "200x200" + urls["suffix"]
+  	else
+  		urls = 'error'
+  	end
   end
 
   def handle_error(response)
