084cf42940b3d053c42aadd499a1e30736ccca99
Alex <alexadeschamps@gmail.com>
2015-03-22 20:24:30 -0400

    cant make job if already have one in business, button only shows up when you can create job

--- tably/app/controllers/jobs_controller.rb
+++ tably/app/controllers/jobs_controller.rb
@@ -6,7 +6,11 @@ class JobsController < ApplicationController
 	end
 
 	def create
-		check_job = Job.find_by({user_id: current_user.id, business_id: params["id"]})
+		if current_user
+			check_job = Job.find_by({user_id: current_user.id, business_id: params["id"]})
+		else
+			flash[:notice] = "You need to be signed in"
+			redirect_to '/businesses/' + params["id"].to_s
 		if check_job
 			flash[:notice] = "You are already an employee here"
 			redirect_to '/businesses/' + params["id"].to_s
@@ -14,7 +18,6 @@ class JobsController < ApplicationController
 			new_job = Job.create({user_id: current_user.id, position: params["position"], business_id: params["id"]})
 			redirect_to '/businesses/' + params[:business_id] + '/jobs/' + new_job.id.to_s
 		end
-		# redirect_to business_job_path(new_job.business, new_job)
 	end
 
 	def new
